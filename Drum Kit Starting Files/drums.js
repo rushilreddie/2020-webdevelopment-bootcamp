90a116c381a2149b5cb8367db651fcc28965b9c1
rushilreddie <rajarushil.reddy@gmail.com>
2020-06-01 19:44:36 -0500

    drum roll code

---
+++ Drum Kit Starting Files/drums.js
@@ -0,0 +1,14 @@
+
+for(var i=0; i< document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll (".drum")[i].addEventListener("click", function(){
+        alert("I got clicked");
+    
+    });
+    
+
+}
+
+
+
+
