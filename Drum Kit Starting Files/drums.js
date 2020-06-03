2e1388e2fdb3fd41e7f363c6345a6aef780da250
rushilreddie <rajarushil.reddy@gmail.com>
2020-06-02 22:10:29 -0500

    drums using keys

--- Drum Kit Starting Files/drums.js
+++ Drum Kit Starting Files/drums.js
@@ -1,56 +1,63 @@
 
 for(var i=0; i< document.querySelectorAll(".drum").length; i++) {
-
+// buttom press on screen
     document.querySelectorAll (".drum")[i].addEventListener("click", function(){
         
 
       var buttonInnerHTML = this.innerHTML;
 
-        switch (buttonInnerHTML) {
-            case "d":
-                
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-        
-                case "j" :
-                    var tom2 = new Audio("sounds/tom-2.mp3");
-                    tom2.play();
-                break;
-                case "w" :
-                    var crash = new Audio("sounds/crash.mp3");
-                    crash.play();
-                break;
-                
-                case "a" :
-                var kickBass = new Audio("sounds/kick-bass.mp3");
-                kickBass.play();
-                break;
-                
-                case "s" :
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            
-                case "k" :
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
+      makeSound(buttonInnerHTML);
 
-                case "l" :
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-
-            default: console.log(buttonInnerHTML);
-                
-        }
-    
     });
     
 
 }
+//keyboard press
+ document.addEventListener("keypress", function(event){
+     makeSound(event.key);
+ })
 
+function makeSound(key) {
+    switch (key) {
+        case "d":
+            
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+    
+            case "j" :
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+            break;
+            case "w" :
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+            break;
+            
+            case "a" :
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+            
+            case "s" :
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+            case "k" :
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
 
+            case "l" :
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+            
+    }
+
+}
 
 
