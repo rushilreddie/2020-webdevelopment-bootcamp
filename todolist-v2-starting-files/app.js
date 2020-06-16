c136ca20dcb29a054290e7fedf475dd82b98b3c4
rushilreddie <rajarushil.reddy@gmail.com>
2020-06-16 13:43:15 -0500

    todolist updated with db

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -1,9 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const _ = require("lodash");
 
 const app = express();
 
@@ -12,29 +10,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
 };
-  
+
 const Item = mongoose.model("Item", itemsSchema);
 
+
 const item1 = new Item({
-        name: "Welcome to your todolist!"
-
+  name: "Welcome to your todolist!"
 });
-const item2 = new Item({
-  name: "Hit + button to add a new item"
 
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
 });
 
 const item3 = new Item({
-  name: "<-- Hit this to delete an item "
+  name: "<-- Hit this to delete an item."
 });
 
+const defaultItems = [item1, item2, item3];
 
-const defaultItems = [item1,item2,item3];
 const listSchema = {
   name: String,
   items: [itemsSchema]
@@ -83,7 +81,6 @@ app.get("/:customListName", function(req, res){
       }
     }
   });
-
 
 
 
