98a4f1bafd756fadd57ba85f0cb46fff82c5e49d
rushilreddie <rajarushil.reddy@gmail.com>
2020-06-17 19:05:37 -0500

    Creating OWN RestAPi

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const { ifError } = require("assert");
 
 const app = express();
 
@@ -20,8 +21,10 @@ const articleSchema = {
 };
 
 const Article = mongoose.model("Article", articleSchema);
+/////////////////////////////target all articles route//////////////////////////
+app.route("/articles")
 
-app.get ("/articles", function(req,res){
+.get(function(req,res){
   Article.find(function(err, foundArticles){
     if (!err)
 {res.send(foundArticles);} 
@@ -29,9 +32,9 @@ app.get ("/articles", function(req,res){
     res.send(err);
   }
   });
-});
+})
 
-app.post ("/articles", function(req,res){
+.post(function(req,res){
 
   const newArticle = new Article({
     title : req.body.title,
@@ -44,9 +47,9 @@ app.post ("/articles", function(req,res){
       res.send(err);
     }
   });
-});
+})
 
-app.delete("/articles", function(req,res){
+.delete(function(req,res){
   Article.deleteMany(function(err){
     if (!err){
       res.send("Successfully deleted all articles");
@@ -55,6 +58,62 @@ app.delete("/articles", function(req,res){
     }
   });
   
+});
+/////////////////////////////target specific article route//////////////////////////
+
+app.route("/articles/:articleTitle")
+
+
+.get(function(req,res){
+
+
+ Article.findOne({title: req.params.articleTitle}, function(err, foundActicle){
+   if(foundActicle){
+     res.send(foundActicle);
+   }else{
+     res.send("No articles matching the title")
+   }
+ });
+})
+.put(function(req,res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article")
+      }
+    }
+  )
+})
+
+.patch(function(req,res){
+  
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body}, 
+    function(err){
+      if(!err){
+        res.send("Successfully updated article")
+      }else{
+        res.send(err);
+      }
+    }
+  );
+})
+
+.delete(function(req,res){
+  Article.deleteOne(    
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted article")
+      }else{
+        res.send(err);
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
