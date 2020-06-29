f72815b045050b9b2ff85c26bd39ff275c69a22e
rushilreddie <rajarushil.reddy@gmail.com>
2020-06-29 18:10:20 -0500

    creating environment for react

--- Login page/app.js
+++ Login page/app.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findorcreate')
+const findOrCreate = require('mongoose-findorcreate');
 
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
